b8d9f797029e0dd94dd60ac4bc14c149b8b63d05
bsc7th <ryanjayleyva@proton.me>
2025-04-06 14:40:29 -0400

    create heroContainer div element

---
+++ calculator.js
@@ -0,0 +1,24 @@
+function hero() {
+  const heroContainer = document.createElement("div");
+  // const heroWrapper = document.createElement("div");
+  //
+  // const title = document.createElement("h1");
+  // title.textContent = "Calculator";
+  //
+  // heroContainer.append(heroWrapper);
+  // heroWrapper.append(title);
+
+  return heroContainer;
+}
+
+function app() {
+  const root = document.getElementById("root");
+
+  if (root) {
+    root.append(hero());
+  } else {
+    console.error("Element with id 'root' not found.");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", app);
